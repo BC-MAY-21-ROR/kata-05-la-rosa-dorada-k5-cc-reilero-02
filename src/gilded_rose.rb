39ec19567afe023ea1f775a39416b184a6edc104
Carlos Zamora <czamora5@ucol.mx>
2022-08-22 19:16:02 -0500

    Refactor update_quality method
    Co-authored-by: Brandon2002 <btapia@ucol.mx>
    Co-authored-by: CalixtoLopez <calixto.lopez.santos@gmail.com>
    Co-authored-by: jorgefloresv75 <jorgefloresv75@yahoo.es>

--- src/gilded_rose.rb
+++ src/gilded_rose.rb
@@ -4,80 +4,77 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
-    def quality_rose(item)
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
+  def update_item_quality(item, value)
+    if item.quality + value <= 50 && item.quality + value >= 0
+      item.quality = item.quality + value
+    end
+  end
+
+  def quality_rose(item)
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+            quality_sulfuras(item)            
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality - item.quality
         end
+      else
+        update_item_quality(item, 1)
       end
     end
+  end
+
+  def quality_sulfuras(item)
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      # item.sell_in = item.sell_in - 1
+      item.quality = item.quality - 1
+    end
+  end
+
+  def update_sell_in(item)
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.quality = item.quality - 1
+    end
+  end
+
+ 
+
+  def update_quality_backstage_passes(item)
+    if item.sell_in < 11
+      update_item_quality(item, 1)
+    end
+    if item.sell_in < 6
+      update_item_quality(item, 1)
+    end
+  end
+
+  def update_quality()
 
     @items.each do |item|
       
+      next if item.name == "Sulfuras, Hand of Ragnaros"
+
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+        update_item_quality(item, -1)
       
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
+        update_item_quality(item, 1)
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          update_quality_backstage_passes(item)
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      update_sell_in(item)
       #Quality < 50 
+
       quality_rose(item)
-      # if item.sell_in < 0
-      #   if item.name != "Aged Brie"
-      #     if item.name != "Backstage passes to a TAFKAL80ETC concert"
-      #       if item.quality > 0
-      #         if item.name != "Sulfuras, Hand of Ragnaros"
-      #           item.quality = item.quality - 1
-      #         end
-      #       end
-      #     else
-      #       item.quality = item.quality - item.quality
-      #     end
-      #   else
-      #     if item.quality < 50
-      #       item.quality = item.quality + 1
-      #     end
-      #   end
-      # end
     end
     
 
   end
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
@@ -92,4 +89,3 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
-
