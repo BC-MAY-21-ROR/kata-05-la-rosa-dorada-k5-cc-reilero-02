805addcd93cced8e4aa13234da6dedc9bcf27ea2
Calixto <calixto.lopez.santos@gmail.com>
2022-08-24 01:24:44 +0200

    Aplication change with Rubocop
    Co-authored-by: Brandon2002 <btapia@ucol.mx>
    Co-authored-by: CZamora5 <czamora5@ucol.mx>
    Co-authored-by: jorgefloresv75 <jorgefloresv75@yahoo.es>

--- src/gilded_rose.rb
+++ src/gilded_rose.rb
@@ -1,85 +1,71 @@
-class GildedRose
+# frozen_string_literal: true
 
+# Method GildedRose, we can selected with the rules items
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_item_quality(item, value)
-    if item.quality + value <= 50 && item.quality + value >= 0
-      item.quality = item.quality + value
+  def quality_rose(item)
+    return unless item.sell_in.negative?
+
+    if item.name != 'Aged Brie'
+      quality_select_rose(item)
+    else
+      update_item_quality(item, 1)
     end
   end
 
-  def quality_rose(item)
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            quality_sulfuras(item)
-            if item.quality > 0 and item.name ==  "Conjured Mana Cake" 
-              quality_sulfuras(item)
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        update_item_quality(item, 1)
-      end
+  def quality_select_rose(item)
+    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+
+      update_item_quality(item, -1)
+
+      update_item_quality(item, -1) if item.name == 'Conjured Mana Cake'
+    else
+      item.quality -= item.quality
     end
+  end
+
+  def update_item_quality(item, value)
+    item.quality = item.quality + value if item.quality + value <= 50 && item.quality + value >= 0
   end
 
   def quality_sulfuras(item)
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.quality = item.quality - 1
-    end
+    item.quality = item.quality - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
   end
 
   def update_sell_in(item)
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
+    item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
   end
-
- 
 
   def update_quality_backstage_passes(item)
-    if item.sell_in < 11
+    update_item_quality(item, 1) if item.sell_in < 11
+    update_item_quality(item, 1) if item.sell_in < 6
+  end
+
+  def update_select_items(item)
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      update_item_quality(item, -1)
+      update_item_quality(item, -1) if item.name == 'Conjured Mana Cake'
+    else
       update_item_quality(item, 1)
-    end
-    if item.sell_in < 6
-      update_item_quality(item, 1)
+      update_quality_backstage_passes(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
     end
   end
 
-  def update_quality()
-
+  def update_quality
     @items.each do |item|
-      
-      next if item.name == "Sulfuras, Hand of Ragnaros"
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
 
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" 
-        update_item_quality(item, -1)
-        if item.name == "Conjured Mana Cake"
-          update_item_quality(item, -1)
-        end
-      else
-        update_item_quality(item, 1)
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          update_quality_backstage_passes(item)
-        end
-      end
+      update_select_items(item)
       update_sell_in(item)
-      #Quality < 50 
-
       quality_rose(item)
     end
-    
-
   end
 end
 
-
+# Name class Item and values
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -89,7 +75,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
